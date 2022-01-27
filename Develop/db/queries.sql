3c4242488191f9358f0ba1b8787b52e3dc2e73c1
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-27 00:52:34 -0500

    CREATED: employee table; ADDED: expectation notes in queries

---
+++ Develop/db/queries.sql
@@ -0,0 +1,7 @@
+-- need to view all depts in depts table
+
+
+-- view all roles
+
+
+-- view all employees
--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE roles (
     job_title VARCHAR(30) NOT NULL,
     salary INT NOT NULL,
 )
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department VARCHAR(30) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
+    first_name VARCHAR(70) NOT NULL,
+    last_name VARCHAR(70) NOT NULL,
+    salary INT NOT NULL,
+    manager VARCHAR(70) NOT NULL,
+)
