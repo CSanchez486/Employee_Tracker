3d738c2dc3e4eef7e7634ebf588453dab2f9dabb
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-27 23:17:17 -0500

    added tables to mysql from schema file

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,26 +5,29 @@ USE e_tracker_db;
 
 CREATE TABLE departments (
     dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department VARCHAR(30) NOT NULL,
-)
+    department VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles (
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department VARCHAR(30) NOT NULL,
+    dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL,
+    departments VARCHAR(30),
     job_title VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
-    FOREIGN KEY (departments)
-    REFERENCES departments(department),
-)
+    salary INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES departments (dept_id)
+);
 
 CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department VARCHAR(30) NOT NULL,
+    dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    e_id INT NOT NULL,
+    departments VARCHAR(30),
     job_title VARCHAR(30) NOT NULL,
+    salary INT,
     first_name VARCHAR(70) NOT NULL,
     last_name VARCHAR(70) NOT NULL,
-    salary INT NOT NULL,
     manager VARCHAR(70) NOT NULL,
-    FOREIGN KEY (roles)
-    REFERENCES (department, salary, job_title)
-)
+    FOREIGN KEY (dept_id),
+    REFERENCES departments (dept_id)
+);
+
