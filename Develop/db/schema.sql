d013ea5956372babdbf3be25f20ec9f68caa9472
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-02 18:38:19 -0500

    refactored the id's from schema.sql and removed auto_increment. seeds.sql was updated to match the new columns from the tables.

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS e_tracker_db;
+ DROP DATABASE IF EXISTS e_tracker_db;
 CREATE DATABASE e_tracker_db;
 
 USE e_tracker_db;
 
 CREATE TABLE departments (
-    dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_id INT NOT NULL PRIMARY KEY,
     department VARCHAR(30) NOT NULL
 );
 
@@ -14,25 +14,27 @@ CREATE TABLE roles (
     department VARCHAR(30),
     job_title VARCHAR(30) NOT NULL,
     salary INT,
-    FOREIGN KEY (dept_id)
-    REFERENCES departments (dept_id)
+    FOREIGN KEY(dept_id)
+    REFERENCES departments(dept_id)
 );
 
--- INDEX d_id (dept_id)
 
 CREATE TABLE employees (
-    e_id INT NOT NULL,
+    e_id INT NOT NULL PRIMARY KEY,
     role_id INT NOT NULL,
+    dept_id INT NOT NULL,
     department VARCHAR(30),
     job_title VARCHAR(30) NOT NULL,
     salary INT,
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
     manager VARCHAR(200) NOT NULL,
+    CONSTRAINT fk_role
     FOREIGN KEY (role_id)
-    REFERENCES roles (role_id)
+    REFERENCES roles(role_id)
+    ON DELETE CASCADE
 );
 
--- INDEX m_id (manager),
+
 
 
--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,27 +1,27 @@
 -- data referenced at: https://www.zinggrid.com/demos/web-component-grid-column-type-range
 
-INSERT INTO departments (department)
-VALUES  ("Delivery Crew"),
-        ("Office Management"),
-        ("Support Staff");
+INSERT INTO departments (dept_id, department)
+VALUES  (123, "Delivery Crew"),
+        (456, "Office Management"),
+        (789, "Support Staff");
 
-INSERT INTO roles (role_id, department, job_title, salary)
-VALUES  (1, "Delivery Crew","Captain", 150000),
-        (2, "Delivery Crew","Delivery Boy", 22500),
-        (3, "Delivery Crew","Ships Robot", 50000),
-        (4, "Office Management", "Owner", 750000 ),
-        (5, "Office Management", "Office Manager", 150000),
-        (6, "Support Staff", "Crew Member/Intern", 29500),
-        (7, "Support Staff", "Staff Doctor", 90000),
-        (8, "Support Staff", "Pet/Fuel Engineer", 0);
+INSERT INTO roles (role_id, dept_id, department, job_title, salary)
+VALUES  (1, 123, "Delivery Crew","Captain", 150000),
+        (2, 123, "Delivery Crew","Delivery Boy", 22500),
+        (3, 123,"Delivery Crew","Ships Robot", 50000),
+        (4, 456,"Office Management", "Owner", 750000 ),
+        (5, 456, "Office Management", "Office Manager", 150000),
+        (6, 789, "Support Staff", "Crew Member/Intern", 29500),
+        (7, 789, "Support Staff", "Staff Doctor", 90000),
+        (8, 789,"Support Staff", "Pet/Fuel Engineer", 0);
 
-INSERT INTO employees(e_id, dept_id, department, job_title, salary, first_name, last_name, manager)
-VALUES  (001100, 314, "Delivery Crew",   "Captain",         150000, "Turanga", "Leela", "Hermes Conrad"),
-        (010010, 314, "Delivery Crew",   "Delivery Boy",    22500, "Philip J.", " Fry", "Turanga Leela"),
-        (011110, 314, "Delivery Crew",   "Ships Robot",     50000, "Bender Bending" "Rodríguez", "Turanga Leela"),
-        (100001, 159, "Office Management", "Owner/Founder", 750000, "Professor Hubert J.", "Farnsworth", "Professor Hubert J. Farnsworth"),
-        (101101, 159,  "Office Management","Office Manager", 150000, "Hermes", "Conrad", "Professor Hubert J. Farnsworth"),
-        (110011, 265,  "Support Staff",   "Crew Member/Intern", 29500, "Amy", "Wong PhD", "Hermes Conrad"),
-        (100000, 265,  "Support Staff",   "Staff Doctor",    90000, "John A.", "Zoidberg MD","Hermes Conrad"),
-        (000001, 265,  "Support Staff",   "Pet/Fuel Engineer", 0, "Lord", "Nibbler" , "Turanga Leela");  
+INSERT INTO employees (e_id, role_id, dept_id, department, job_title, salary, first_name, last_name, manager)
+VALUES  (001100, 1, 123, "Delivery Crew", "Captain", 150000, "Turanga", "Leela", "Hermes Conrad"),
+        (010010, 2, 123, "Delivery Crew", "Delivery Boy", 22500, "Philip J.", " Fry", "Turanga Leela"),
+        (011110, 3, 123, "Delivery Crew", "Ships Robot", 50000, "Bender Bending", "Rodríguez", "Turanga Leela"),
+        (100001, 4,456, "Office Management", "Owner/Founder", 750000, "Professor Hubert J.", "Farnsworth", "Professor Hubert J. Farnsworth"),
+        (101101, 5, 456,"Office Management","Office Manager", 150000, "Hermes", "Conrad", "Professor Hubert J. Farnsworth"),
+        (110011, 6, 789, "Support Staff", "Crew Member/Intern", 29500, "Amy", "Wong PhD", "Hermes Conrad"),
+        (100000, 7, 789, "Support Staff", "Staff Doctor", 90000, "John A.", "Zoidberg MD","Hermes Conrad"),
+        (000001, 8, 789, "Support Staff", "Pet/Fuel Engineer", 0, "Lord", "Nibbler" , "Turanga Leela");  
    
